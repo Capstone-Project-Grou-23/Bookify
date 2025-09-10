87f930031386a079a9ce9215cbbac72134254dba
Shivam Singh <sudouseronpre6@gmail.com>
2025-09-10 09:52:02 +0530

    Update App.js

--- code/src/App.js
+++ code/src/App.js
@@ -1,16 +1,54 @@
+// src/App.js
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Signup from "./Signup";  // make sure Signup.js exists
-import Login from "./Login";    // make sure Login.js exists
-import Home from "./Home";      // optional homepage
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+// Main Setting Page
+import Setting from "./components/setting/Setting";
+
+// Setting Options
+import ProfileSettings from "./components/setting/setting-options/ProfileSettings";
+import AccountSettings from "./components/setting/setting-options/AccountSettings";
+import PreferencesSettings from "./components/setting/setting-options/PreferencesSettings";
+import PrivacySettings from "./components/setting/setting-options/PrivacySettings";
+import NotificationSettings from "./components/setting/setting-options/NotificationSettings";
+import SupportSetting from "./components/setting/setting-options/SupportSetting";
+
+// Support Sub-options
+import Help from "./components/setting/setting-options/support-option/Help";
+import Contact from "./components/setting/setting-options/support-option/Contact";
+import About from "./components/setting/setting-options/support-option/About";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<Signup />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    <Router>
+      <Routes>
+        {/* Core Pages */}
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+
+        {/* Settings Main */}
+        <Route path="/settings" element={<Setting />} />
+
+        {/* Setting Options */}
+        <Route path="/settings/profile" element={<ProfileSettings />} />
+        <Route path="/settings/account" element={<AccountSettings />} />
+        <Route path="/settings/preferences" element={<PreferencesSettings />} />
+        <Route path="/settings/privacy" element={<PrivacySettings />} />
+        <Route path="/settings/notifications" element={<NotificationSettings />} />
+        <Route path="/settings/support" element={<SupportSetting />} />
+
+        {/* Support Sub-options */}
+        <Route path="/settings/support/help" element={<Help />} />
+        <Route path="/settings/support/contact" element={<Contact />} />
+        <Route path="/settings/support/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
