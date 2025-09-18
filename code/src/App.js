8597ee6987f9f9c32cdd424aa4f91fc898b5f03e
Shivam Singh <sudouseronpre6@gmail.com>
2025-09-18 18:41:32 +0530

    Update App.js

--- code/src/App.js
+++ code/src/App.js
@@ -1,37 +1,46 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom";
 import Login from "./components/login/Login";
 import Signup from "./components/signup/Signup";
 import Navbar from "./components/homepage/Navbar";
 import Hero from "./components/homepage/Hero";
+import Footer from "./components/homepage/Footer";
 import Profile from "./components/profile/Profile";
-import Setting from "./components/setting/Setting"; 
+import Setting from "./components/setting/Setting";
 import Buy from "./components/buy/Buy";
 import Sell from "./components/sell/Sell";
-import Footer from "./components/homepage/Footer"; // Import Footer
 
-function HomePage() {
-  return (
-    <div>
-      <Navbar />
-      <Hero />
-      <Footer /> {/* Add Footer here */}
-    </div>
-  );
-}
+// Layout component to wrap pages with Navbar and Footer
+const PageLayout = () => (
+  <>
+    <Navbar />
+    <main style={{ flex: '1', padding: '20px 0' }}>
+      <Outlet />
+    </main>
+    <Footer />
+  </>
+);
+
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/setting" element={<Setting />} />
-        <Route path="/buy" element={<Buy />} />
-        <Route path="/sell" element={<Sell />} />
-      </Routes>
+       <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          
+          {/* Routes with Navbar and Footer */}
+          <Route element={<PageLayout />}>
+            <Route path="/" element={<Hero />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/settings" element={<Setting />} />
+            <Route path="/buy" element={<Buy />} />
+            <Route path="/sell" element={<Sell />} />
+          </Route>
+
+        </Routes>
+      </div>
     </Router>
   );
 }
