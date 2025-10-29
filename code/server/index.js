abdfe0d7dca4fd4457a1256dd46d447d658e82ca
Shivam Singh <sudouseronpre6@gmail.com>
2025-10-29 20:11:12 +0530

    Update index.js

--- code/server/index.js
+++ code/server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require("express");
+const passport = require('./auth/passport-config');
 const mysql = require("mysql2");
 const cors = require("cors");
 const bodyParser = require("body-parser");
