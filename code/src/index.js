b9e1d1e38af8ddb239e3bea922c816f1ad17281d
Shivam Singh <sudouseronpre6@gmail.com>
2025-11-05 20:40:36 +0530

    Wrap App component with Theme and Cart providers

--- code/src/index.js
+++ code/src/index.js
@@ -4,13 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// 1. Import your providers
+import { ThemeProvider } from './ThemeContext';
+import { CartProvider } from './context/CartContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 2. Wrap your App with both providers */}
+    <ThemeProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
-
 
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
