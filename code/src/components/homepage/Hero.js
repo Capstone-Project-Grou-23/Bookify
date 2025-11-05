0441657051dad882db3fc381d96ff9d3e1cc950e
Shivam Singh <sudouseronpre6@gmail.com>
2025-11-05 20:43:39 +0530

    Update Hero.js

--- code/src/components/homepage/Hero.js
+++ code/src/components/homepage/Hero.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Hero.css";
 import { Link } from "react-router-dom";
+// 1. Import FontAwesome and the new icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHandHoldingHeart, faHandHoldingUsd } from "@fortawesome/free-solid-svg-icons";
 
 const Hero = () => {
   return (
@@ -8,8 +11,17 @@ const Hero = () => {
       <h1>Welcome to Bookify</h1>
       <p>Your modern marketplace for books</p>
       <div className="buttons">
-        <Link to="/buy" className="btn-green">BUY</Link>
-        <Link to="/sell" className="btn-orange">SELL</Link>
+        {/* 2. Update the BUY button */}
+        <Link to="/buy" className="btn-green hero-btn">
+          <FontAwesomeIcon icon={faHandHoldingHeart} className="hero-icon" />
+          <span>BUY</span>
+        </Link>
+        
+        {/* 3. Update the SELL button */}
+        <Link to="/sell" className="btn-orange hero-btn">
+          <FontAwesomeIcon icon={faHandHoldingUsd} className="hero-icon" />
+          <span>SELL</span>
+        </Link>
       </div>
     </div>
   );
